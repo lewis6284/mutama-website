95b8bd478eaac8efea3d68b4f576e2fd24c5b629
lewis6284 <lewisirakoz@gmail.com>
2026-04-26 19:53:18 +0200

    i resolve the server runing errors

--- app.js
+++ app.js
@@ -29,10 +29,15 @@ for (const page of htmlPages) {
   });
 }
 
-app.get("/{*path}", (_req, res) => {
+// Catch-all route for SPA - must be the last route
+app.use((_req, res) => {
   res.sendFile(path.join(publicDir, "index.html"));
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
 });
